52c961e98363b325d915565d7fc746f8ea6810cc
Abel <abelderviche@gmail.com>
2019-05-29 23:43:38 -0300

    detail

--- src/components/Hotel/Summary/detail.jsx
+++ src/components/Hotel/Summary/detail.jsx
@@ -10,7 +10,36 @@ const getStars = (stars) =>{
     return starsArr;
 }
 
+const detailInfo = (label,icon,value) => (
+    <div className="info">
+        <div className="title"><div className="icon"></div>{label}</div>
+        <div className="label">{value}</div>
+    </div>
+)
+
 const Detail = ({name,stars,image,address,checkin,checkout,nights,rooms,mealPlan,roomDetail,amenities}) => {
+    let childAges = [];
+   let adultsQty = 0;
+   let childsQty = 0;
+   let infantsQty = 0;
+
+    for (let room of rooms) {
+        if (room.child_ages) {
+        childAges.push(...room.child_ages)
+        adultsQty += room.capacity - room.child_ages.length
+        } else {
+        adultsQty += room.capacity
+        }
+    }
+    
+    for (let childAge of childAges){
+        if(childAge>3){
+            childsQty++;
+        }else{
+            infantsQty++;
+        }
+    }
+
     return (
         <div className="hotel-box">
             <div className="hotel-box__section description">
@@ -38,7 +67,7 @@ const Detail = ({name,stars,image,address,checkin,checkout,nights,rooms,mealPlan
                 */}
             </div>
             <div className="hotel-box__section  date">
-                <div><svg className="icon-location">
+                <div><svg className="icon-date">
                                 <use xlinkHref={`${require('assets/img/sprite.svg')}#icon-date`}></use>
                         </svg></div>
                 <div>{checkin}</div>
@@ -46,6 +75,13 @@ const Detail = ({name,stars,image,address,checkin,checkout,nights,rooms,mealPlan
                     <img src={require('assets/img/arrow-reservas.png')} alt=""/>
                 </div>
                 <div>{checkout}</div>
+            </div>
+            <div className="hotel-box__section  guestrooms">
+                {detailInfo('Adultos','',adultsQty)}
+                {detailInfo('Niños','',childsQty)}
+                {detailInfo('Bebes','',infantsQty)}
+                {detailInfo('Habitaciones','',rooms.length)}
+                {detailInfo('Tipo','','Standard')}
             </div>
         </div>
     )
